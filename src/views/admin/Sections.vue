f92449e132c33ab90fca00bf2172638d1c164253
mahmad-labib <mohamed.anwarlabib@gmail.com>
2021-11-16 16:54:42 +0200

    d

--- src/views/admin/Sections.vue
+++ src/views/admin/Sections.vue
@@ -7,7 +7,7 @@
     <v-row>
       <v-col>
         <v-treeview
-          v-model="sectionsSelection"
+          v-model="selection"
           :items="sections"
           :selection-type="selectionType"
           selectable
@@ -16,16 +16,11 @@
       </v-col>
       <v-divider vertical></v-divider>
       <v-col class="pa-6" cols="6">
-        <template v-if="!sectionsSelection.length">
-          No nodes selected.
-        </template>
+        <template v-if="!selection.length"> No nodes selected. </template>
         <template v-else>
-          <div v-for="node in sectionsSelection" :key="node.id">
+          <div v-for="node in selection" :key="node.id">
             <div
-              v-if="
-                sectionsSelection.find((o) => o.id === node.parentId) ==
-                undefined
-              "
+              v-if="selection.find((o) => o.id === node.parentId) == undefined"
             >
               {{ node.name }}
             </div>
@@ -38,9 +33,24 @@
 
 <script>
 import { mapState } from "vuex";
+import store from "../../store";
 
 export default {
   name: "Sections",
+  data() {
+    return {
+      selection: [],
+      selectionType: "independent",
+    };
+  },
+  methods: {
+    get_sections() {
+      return store.dispatch("get_sections");
+    },
+  },
+  mounted() {
+    this.get_sections();
+  },
   computed: {
     ...mapState({
       sections: (state) => state.sections.sections,
